70d722a0eea2b7fe50c156e9973400b31476d116
patelaftab <10patelaftab@gmail.com>
2023-02-01 09:58:55 +0530

    [Aftab] Added New UCs

---
+++ DemoProject/DemoProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using DemoProject;
+using System;
+
+
+namespace Demo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Welcome to Demo");
+            Console.WriteLine("======================================================");
+            UCs.FirstUcs();
+            UCs uCs=new UCs();
+            uCs.SecondUc(); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoProject/DemoProject/UCs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    public class UCs
+    {
+        public static void FirstUcs()
+        {
+            Console.WriteLine("UC1");
+        }
+        public void SecondUc()
+        {
+            Console.WriteLine("UC2");
+        }
+    }
+}
